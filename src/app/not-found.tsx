d6e81ce11d3d1d06a06528f6e04c7f9529186a31
LunaUrsa <1836049+LunaUrsa@users.noreply.github.com>
2023-10-28 21:19:03 -0500

    Redirect working

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { RedirectType, permanentRedirect } from "next/navigation";
+import { headers } from "next/headers";
+
+async function fetchUrl(url: string): Promise<boolean> {
+  try {
+    const res = await fetch(url);
+    return res.ok;
+  } catch (error) {
+    return false;
+  }
+}
+
+export default async function NotFound() {
+  const referer = headers().get("referer");
+  if (referer) {
+    const blogUrl = referer.replace("https://", "https://blog.");
+    if (await fetchUrl(blogUrl)) {
+      permanentRedirect(blogUrl, "replace" as RedirectType);
+    }
+  }
+  return (
+    <div className="global">
+      <h2>Not Found </h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}

2db277bde38570cefadd8a245d580271f25a8f42
Alana E <alanaaaliaemery@gmail.com>
2025-02-01 01:36:58 -0700

    chore(Head): Improve meta tag formatting
    Reformatted the description meta tag for better readability and consistency in the Head component.

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,7 +6,10 @@ function MyHead() {
       <title>TripSit.Me</title>
       <meta charSet="utf-8" />
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-      <meta content="Tripsit Harm Reduction Through Education" name="description" />
+      <meta
+        content="Tripsit Harm Reduction Through Education"
+        name="description"
+      />
       <meta content="relevant, keywords, here" name="keywords" />
       <meta name="robots" content="all" />
       <link rel="icon" href="/favicon.ico" />

96df7431821a1aa373d1124bb878fc6556a605f7
theimperious1 <shadow@tripsit.me>
2025-08-27 12:53:26 -0400

    try fix fading out login area

--- src/pages/appeal.tsx
+++ src/pages/appeal.tsx
@@ -378,12 +378,8 @@ const AppealPage: React.FC = () => {
       <Header />
       <main>
         {!token && (
-          <section id="hero" className="d-flex align-items-center">
-            <div
-              className="container position-relative"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
+          <section className="py-5">
+            <div className="container position-relative">
               <div className="row justify-content-center">
                 <div className="col-xl-7 col-lg-9 text-center">
                   <h1>Ban Appeal</h1>
